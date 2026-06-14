bf98cc8a847211aa656ae3411b3e1f828beac823
visatk <visatk29@gmail.com>
2026-06-14 22:25:45 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const response = await fetch("https://api.example.com/large-dataset");
+    return new Response(response.body, response);
+  },
+} satisfies ExportedHandler<Env>;
